3de3762cee19951d4f2247d8a8e0c7f65df4892a
Mark Holton <holtonma@Macintosh-69.local>
2009-03-01 16:43:49 -0800

    need more tests...

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,8 +2,8 @@ class GroupsController < ApplicationController
   # GET /groups
   # GET /groups.xml
   def index
-    @groups = Group.find(:all)
-    #@groups = Group.find(:all, :include => :locations, :limit => 10, :order => 'groups.name ASC') 
+    #@groups = Group.find(:all)
+    @groups = Group.find(:all, :include => :locations, :limit => 10, :order => 'groups.name ASC') 
     #@groups = Group.find(:all, :include => [:locations, :events], :limit => 10, :order => 'groups.name ASC')   
     respond_to do |format|
       format.html # index.html.erb
