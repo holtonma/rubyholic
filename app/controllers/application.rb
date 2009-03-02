2aa98f29600f18d97e4bb4f3fc0d7461291085e7
Mark Holton <holtonma@Macintosh-69.local>
2009-03-01 19:44:03 -0800

    keeping sorting DRY

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,4 +12,11 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  
+  def order_by
+    return {} if params[:order].blank?
+    direction = params[:direction] || "ASC"
+    { :order => "#{params[:order]} #{direction}" }
+  end
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def sortable_heading( label, field, make_link )
+    up_params = { :order => field, :direction => 'asc' }
+    down_params = { :order => field, :direction => 'desc' }
+    "%s #{label} %s" % [
+      link_to( '^', make_link.call( up_params ) ),
+      link_to( 'v', make_link.call( down_params ) )
+    ]
+  end
+  
 end
