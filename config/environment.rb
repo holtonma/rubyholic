74268aa7db22cc195cfe28dc137ad8520d021426
Mark Holton <holtonma@Macintosh-69.local>
2009-03-22 16:32:04 -0700

    geokit installed

--- config/environment.rb
+++ config/environment.rb
@@ -44,7 +44,9 @@ Rails::Initializer.run do |config|
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
   config.time_zone = 'UTC'
-
+  
+  config.gem "geokit"
+  
   # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
   # All files from config/locales/*.rb,yml are added automatically.
   # config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
